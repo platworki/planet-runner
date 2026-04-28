49bb4398b93ecaec045a4edfdf9ea34d5b124759
Platworki <platworki@gmail.com>
2026-04-28 22:19:36 +0200

    options removed

--- scripts/menu/menu.gd
+++ scripts/menu/menu.gd
@@ -1,7 +1,6 @@
 extends Control
 
 @onready var start: TextureButton = $Start
-@onready var options: TextureButton = $Options
 @onready var exit: TextureButton = $Exit
 
 func _on_play_pressed() -> void:
@@ -10,7 +9,6 @@ func _on_play_pressed() -> void:
 	
 	start.texture_normal = start.texture_hover
 	start.disabled = true
-	options.disabled = true
 	exit.disabled = true
 	SceneTransitions.fade_to_scene_white("res://scenes/world.tscn")
 	
